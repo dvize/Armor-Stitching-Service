d664db95ff64b038eee0b110fb964d963493a4a9
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2023-05-26 21:37:34 +0200

    Revert "Update to 3.5.6 and for components"
    This reverts commit 278f2ff0ea8d96ec9bc07df1d5685b2bb0c9bf76.

--- Plugin.cs
+++ Plugin.cs
@@ -1,75 +1,172 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Reflection;
-using Aki.Reflection.Patching;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Configuration;
+using Comfort.Common;
 using EFT;
-using VersionChecker;
+using EFT.InventoryLogic;
+using HarmonyLib;
+using UnityEngine;
 
-namespace ASS
+namespace armorMod
 {
-    [BepInPlugin("com.dvize.ASS", "dvize.ASS", "1.1.0")]
-    public class Plugin : BaseUnityPlugin
+    [BepInPlugin("com.armorMod.ASS", "armorMod.ASS", "1.0.0")]
+
+    public class ASS : BaseUnityPlugin
     {
-        internal static ConfigEntry<Boolean> ArmorServiceMode { get; set; }
-        internal static ConfigEntry<float> TimeDelayRepairInSec { get; set; }
-        internal static ConfigEntry<float> ArmorRepairRateOverTime { get; set; }
+        private ConfigEntry<Boolean> ArmorServiceMode
+        {
+            get; set;
+        }
+        private static ConfigEntry<float> TimeDelayRepairInSec
+        {
+            get; set;
+        }
+        private static ConfigEntry<float> ArmorRepairRateOverTime
+        {
+            get; set;
+        }
+
+        private AbstractGame game;
+        private bool runOnceAlready = false;
+        private bool newGame = true;
+        private float newRepairRate;
+        private ArmorComponent armor;
+        private static float timeSinceLastHit = 0f;
+
+        private readonly Dictionary<EquipmentSlot, List<Item>> equipmentSlotDictionary = new Dictionary<EquipmentSlot, List<Item>>
+        {
+            { EquipmentSlot.ArmorVest, new List<Item>() },
+            { EquipmentSlot.TacticalVest, new List<Item>() },
+            { EquipmentSlot.Eyewear, new List<Item>() },
+            { EquipmentSlot.FaceCover, new List<Item>() },
+            { EquipmentSlot.Headwear, new List<Item>() }
+        };
 
         internal void Awake()
         {
-            CheckEftVersion();
+            ArmorServiceMode = Config.Bind("Armor Repair Settings", "Enable/Disable Mod", true, "Enables the Armor Repairing Options Below");
+            TimeDelayRepairInSec = Config.Bind("Armor Repair Settings", "Time Delay Repair in Sec", 60f, "How Long Before you were last hit that it repairs armor");
+            ArmorRepairRateOverTime = Config.Bind("Armor Repair Settings", "Armor Repair Rate", 0.5f, "How much durability per second is repaired");
+        }
+        private void Update()
+        {
+            try
+            {
+                game = Singleton<AbstractGame>.Instance;
 
-            ArmorServiceMode = Config.Bind(
-                "Armor Repair Settings",
-                "Enable/Disable Mod",
-                true,
-                "Enables the Armor Repairing Options Below"
-            );
-            TimeDelayRepairInSec = Config.Bind(
-                "Armor Repair Settings",
-                "Time Delay Repair in Sec",
-                60f,
-                "How Long Before you were last hit that it repairs armor"
-            );
-            ArmorRepairRateOverTime = Config.Bind(
-                "Armor Repair Settings",
-                "Armor Repair Rate",
-                0.5f,
-                "How much durability per second is repaired"
-            );
+                if (game.InRaid && Camera.main.transform.position != null && newGame && ArmorServiceMode.Value)
+                {
+                    var player = Singleton<GameWorld>.Instance.MainPlayer;
+                    timeSinceLastHit += Time.deltaTime;
+
+                    if (!runOnceAlready && game.Status == GameStatus.Started)
+                    {
+                        Logger.LogDebug("ASS: Attaching events");
+                        player.BeingHitAction += Player_BeingHitAction;
+                        player.OnPlayerDeadOrUnspawn += Player_OnPlayerDeadOrUnspawn;
+                        runOnceAlready = true;
+                    }
+
+                    RepairArmor();
+                }
+            }
+            catch { }
         }
 
-        private void CheckEftVersion()
+        private void RepairArmor()
         {
-            // Make sure the version of EFT being run is the correct version
-            int currentVersion = FileVersionInfo
-                .GetVersionInfo(BepInEx.Paths.ExecutablePath)
-                .FilePrivatePart;
-            int buildVersion = TarkovVersion.BuildVersion;
-            if (currentVersion != buildVersion)
+            //if the time since we were last hit exceeds TimeDelayRepairInSec.Value then repair all armor
+            if (timeSinceLastHit >= TimeDelayRepairInSec.Value)
             {
-                Logger.LogError(
-                    $"ERROR: This version of {Info.Metadata.Name} v{Info.Metadata.Version} was built for Tarkov {buildVersion}, but you are running {currentVersion}. Please download the correct plugin version."
-                );
-                EFT.UI.ConsoleScreen.LogError(
-                    $"ERROR: This version of {Info.Metadata.Name} v{Info.Metadata.Version} was built for Tarkov {buildVersion}, but you are running {currentVersion}. Please download the correct plugin version."
-                );
-                throw new Exception($"Invalid EFT Version ({currentVersion} != {buildVersion})");
+                //Logger.LogInfo($"Repairing Armor Block Reached Because TimeSinceLastHitReached: " + timeSinceLastHit);
+                //repair the armor divided by the time.unfixed rate
+                newRepairRate = ArmorRepairRateOverTime.Value * Time.deltaTime;
+
+                foreach (EquipmentSlot slot in equipmentSlotDictionary.Keys.ToArray())
+                {
+                    Logger.LogInfo("ASS: Checking EquipmentSlot: " +  slot);
+                    Slot tempSlot = getEquipSlot(slot);
+
+                    if (tempSlot == null || tempSlot.ContainedItem == null)
+                    {
+                        continue;
+                    }
+
+                    foreach (var item in tempSlot.ContainedItem.GetAllItems())
+                    {
+                        //get the armorcomponent of each item in items and check to see if all item componenets (even helmet side ears) are max durability
+                        armor = item.GetItemComponent<ArmorComponent>();
+
+                        //check if it needs repair for the current item in loop of all items for the slot
+                        if (armor != null && (armor.Repairable.Durability < armor.Repairable.MaxDurability))
+                        {
+                            //increase armor durability by newRepairRate until maximum then set as maximum durability
+                            if (armor.Repairable.Durability + newRepairRate >= armor.Repairable.MaxDurability)
+                            {
+                                armor.Repairable.Durability = armor.Repairable.MaxDurability;
+                                //Logger.LogInfo("ASS: Setting MaxDurability for " + item.LocalizedName());
+                            }
+                            else
+                            {
+                                armor.Repairable.Durability += newRepairRate;
+                                //Logger.LogInfo("ASS: Repairing " + item.LocalizedName() + " : " + armor.Repairable.Durability + "/" + armor.Repairable.MaxDurability);
+                            }
+                        }
+
+                    }
+
+                }
             }
         }
-    }
 
-    //re-initializes each new game
-    internal class NewGamePatch : ModulePatch
-    {
-        protected override MethodBase GetTargetMethod() =>
-            typeof(GameWorld).GetMethod(nameof(GameWorld.OnGameStarted));
+        private void Player_BeingHitAction(DamageInfo dmgInfo, EBodyPart bodyPart, float hitEffectId) => timeSinceLastHit = 0f;
 
-        [PatchPrefix]
-        public static void PatchPrefix()
+        private void Player_OnPlayerDeadOrUnspawn(Player player)
         {
-            ASS.ArmorRegenComponent.Enable();
+            Logger.LogDebug("ASS: Undo all events");
+            player.BeingHitAction -= Player_BeingHitAction;
+            player.OnPlayerDeadOrUnspawn -= Player_OnPlayerDeadOrUnspawn;
+            runOnceAlready = false;
+            newGame = false;
+
+            Task.Delay(TimeSpan.FromSeconds(15)).ContinueWith(_ =>
+            {
+                // Set newGame = true after the timer is finished so it doesn't execute the events right away
+                newGame = true;
+            });
         }
+
+        private Slot slotContents;
+        private InventoryControllerClass inventoryController;
+        private Slot getEquipSlot(EquipmentSlot slot)
+        {
+            var player = Singleton<GameWorld>.Instance.MainPlayer;
+
+            // Use AccessTools to get the protected field _inventoryController
+            inventoryController = (InventoryControllerClass)AccessTools.Field(typeof(Player), "_inventoryController").GetValue(player);
+
+
+            if (inventoryController != null)
+            {
+                slotContents = inventoryController.Inventory.Equipment.GetSlot(slot);
+
+                if (slotContents.ContainedItem == null)
+                {
+                    return null;
+                }
+
+                return slotContents;
+            }
+
+            return null;
+        }
+
+        
     }
+
 }
+
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using VersionChecker;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dvize.ASS")]
+[assembly: AssemblyTitle("armorMod.ASS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dvize.ASS")]
-[assembly: AssemblyCopyright("Copyright �  2023")]
+[assembly: AssemblyProduct("armorMod.ASS")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +31,5 @@ using VersionChecker;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: TarkovVersion(23043)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
